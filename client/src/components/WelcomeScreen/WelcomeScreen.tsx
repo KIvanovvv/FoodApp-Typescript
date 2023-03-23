e87c66973a4acb9d57eb72f00c3d0c9058af6aff
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-03-23 13:41:33 +0200

    Improving WelcomeScreen layout

--- client/src/components/WelcomeScreen/WelcomeScreen.tsx
+++ client/src/components/WelcomeScreen/WelcomeScreen.tsx
@@ -1,13 +1,34 @@
 import classes from "./WelcomeScreen.module.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDrumstickBite } from "@fortawesome/free-solid-svg-icons";
+import pizzaImg from "../../resources/pizza.png";
+import { useState } from "react";
 const WelcomeScreen = () => {
+  const [selectionVisible, setSelectionVisible] = useState(false);
   return (
     <div className={classes.wrapper}>
       <div className={classes.header}>
-        <p>Welocme to FoodApp !</p>
-        <p>Delicious food await's you within few clicks</p>
+        <div className={classes.main_header_container}>
+          <p className={classes.main_header}>Welocme to FoodApp</p>
+          <FontAwesomeIcon
+            icon={faDrumstickBite}
+            className={classes.icon}
+            size={"3x"}
+          />
+        </div>
+        <p className={classes.secondary_header}>
+          Order delicious food with few clicks
+        </p>
       </div>
-      <div className={classes.main}></div>
+      <div className={classes.main}>
+        {!selectionVisible && (
+          <img
+            src={pizzaImg}
+            className={classes.pizza}
+            onClick={() => setSelectionVisible(true)}
+          />
+        )}
+      </div>
     </div>
   );
 };
