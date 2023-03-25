d670529b7426e7c7015a7a242f23d1641685c84c
Shtono <shtonojs@gmail.com>
2023-03-25 21:44:38 +0200

    Fixed tht initial TS error. Proposed alternative structure for the CartContext.Created another alternative file structer for using context in contextWithReducer folder inside the context folder.I did not have time to test it.I hope it works fine.If you want to use it import default CartContextProvider from the index file and wrap your components.Then go to any of its childern import useContext from react and CartContext from the same index file.Cart context returns {items, action}.Easy to use and mainatain, easy to scale up.§

---
+++ client/src/context/contextWithReducer/index.ts
@@ -0,0 +1,5 @@
+import CartContextProvider from './context'
+import { CartContext } from './context'
+
+export { CartContext }
+export default CartContextProvider
---
+++ client/src/context/contextWithReducer/types.ts
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+import { actions } from './actions'
+import { ADD, REMOVE, CLEAR } from './constants'
+
+type Actions = ReturnType<typeof actions>
+
+export type ReducerAction = {
+    type: typeof ADD
+    payload: CartItem;
+} | {
+    type: typeof REMOVE
+    payload: string;
+} | {
+    type: typeof CLEAR
+    payload: null;
+}
+
+export type CartItem = {
+  _id: string;
+  itemName: string;
+  price: number;
+};
+
+export interface CartContextInterface {
+  items: CartItem[];
+  actions: Actions
+}
+
+export type CartProvideProps = {
+  children: ReactNode;
+};
