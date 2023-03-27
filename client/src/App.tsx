f921da8617059941a8a7e1583ad610a016fca3f8
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-03-27 11:43:55 +0300

    h

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,18 @@ import { Route, Routes } from "react-router";
 import Menu from "./components/Menu/Menu";
 import RestaurantPage from "./components/RestaurantPage/RestaurantPage";
 import WelcomeScreen from "./components/WelcomeScreen/WelcomeScreen";
+// import CartContextProvider from "./context/contextWithReducer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<WelcomeScreen />} />
-      <Route path="/menu" element={<Menu />} />
-      <Route path="/restaurant/:resId" element={<RestaurantPage />} />
-    </Routes>
+    // <CartContextProvider>
+      <Routes>
+        <Route path="/" element={<WelcomeScreen />} />
+
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/restaurant/:resId" element={<RestaurantPage />} />
+      </Routes>
+    // </CartContextProvider>
   );
 }
 
--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -1,8 +1,13 @@
 import Header from "../Header/Header";
 import Restaurants from "./Restaurants/Restaurants";
 import classes from "./Menu.module.css";
+import { useContext } from "react";
+// import { CartContext } from "../../context/contextWithReducer";
 
 const Menu = () => {
+  // const { items } = useContext(CartContext);
+  // console.log(items);
+
   return (
     <div className={classes.wrapper}>
       <Header />
