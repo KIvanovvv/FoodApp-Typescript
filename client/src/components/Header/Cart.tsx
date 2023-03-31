7e9cd458065f26d8a782e15115c672e7d1f3f3b9
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-03-31 19:32:20 +0300

    Adding CartModal

--- client/src/components/Header/Cart.tsx
+++ client/src/components/Header/Cart.tsx
@@ -1,10 +1,12 @@
 import classes from "./Cart.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../../context/contextWithReducer";
+import CartModal from "./CartModal";
 
 const Cart = () => {
+  const [showModal, setShowModal] = useState(false);
   const { items, actions } = useContext(CartContext);
   const onRemoveFromCart = (itemName: string) => {
     actions.removeItem(itemName);
@@ -16,50 +18,63 @@ const Cart = () => {
   };
 
   const total = items.reduce((a, b) => a + b.price, 0);
- 
+
+  const handleCheckout = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div className={classes.container}>
-      <p className={classes.headline}>Cart</p>
-      <ul className={classes.ul}>
-        {items.length > 0 &&
-          items.map((x) => (
-            <li className={classes.li} key={x.itemName}>
-              <div className={classes.info_container}>
-                <p className={classes.name}>{x.itemName}</p>
-                <p className={classes.price}>{x.price}$</p>
-              </div>
-              <div className={classes.actions}>
-                <p className={classes.counter}>{x.quantity}</p>
-                <FontAwesomeIcon
-                  icon={faMinus}
-                  className={classes.icon}
-                  onClick={() => {
-                    onRemoveFromCart(x.itemName);
-                  }}
-                />
-                <FontAwesomeIcon
-                  icon={faPlus}
-                  className={classes.icon}
-                  onClick={() => {
-                    onAddFromCart(x.itemName);
-                  }}
-                />
-              </div>
-            </li>
-          ))}
-      </ul>
-      {items.length > 0 && (
-        <div className={classes.footer}>
-          <div>
-            <p className={classes.total}>Total amount: {total.toFixed(2)}$</p>
+    <>
+      {showModal && <CartModal closeModal={closeModal} total={total}/>}
+      <div className={classes.container}>
+        <p className={classes.headline}>Cart</p>
+        <ul className={classes.ul}>
+          {items.length > 0 &&
+            items.map((x) => (
+              <li className={classes.li} key={x.itemName}>
+                <div className={classes.info_container}>
+                  <p className={classes.name}>{x.itemName}</p>
+                  <p className={classes.price}>{x.price}$</p>
+                </div>
+                <div className={classes.actions}>
+                  <p className={classes.counter}>{x.quantity}</p>
+                  <FontAwesomeIcon
+                    icon={faMinus}
+                    className={classes.icon}
+                    onClick={() => {
+                      onRemoveFromCart(x.itemName);
+                    }}
+                  />
+                  <FontAwesomeIcon
+                    icon={faPlus}
+                    className={classes.icon}
+                    onClick={() => {
+                      onAddFromCart(x.itemName);
+                    }}
+                  />
+                </div>
+              </li>
+            ))}
+        </ul>
+        {items.length > 0 && (
+          <div className={classes.footer}>
+            <div>
+              <p className={classes.total}>Total amount: {total.toFixed(2)}$</p>
+            </div>
+            <button className={classes.btn_checkout} onClick={handleCheckout}>
+              Checkout
+            </button>
           </div>
-          <button className={classes.btn_checkout}>Checkout</button>
-        </div>
-      )}
-      {items.length === 0 && (
-        <p className={classes.empty}>Your cart is empty</p>
-      )}
-    </div>
+        )}
+        {items.length === 0 && (
+          <p className={classes.empty}>Your cart is empty</p>
+        )}
+      </div>
+    </>
   );
 };
 
---
+++ client/src/components/Header/CartModal.tsx
@@ -0,0 +1,63 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/contextWithReducer";
+import classes from "./CartModal.module.css";
+const CartModal: React.FC<{ closeModal: () => void; total: number }> = (
+  props
+) => {
+  const { items } = useContext(CartContext);
+  return (
+    <>
+      <div className={classes.backdrop} onClick={props.closeModal}>
+        {" "}
+      </div>
+      <div className={classes.main}>
+        <p className={classes.headline}>Proceed with checkout</p>
+        <div className={classes.from}>
+          <div className={classes.input}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+          <div className={classes.input}>
+            <label htmlFor="street">Street</label>
+            <input type="text" id="street" />
+          </div>
+          <div className={classes.input}>
+            <label htmlFor="postal">Postal Code</label>
+            <input type="text" id="postal" />
+          </div>
+          <div className={classes.input}>
+            <label htmlFor="city">City</label>
+            <input type="text" id="city" />
+          </div>
+          <div className={classes.input}>
+            <label htmlFor="phone">Phone number</label>
+            <input type="number" id="phone" />
+          </div>
+        </div>
+        <div className={classes.order}>
+          <ul className={classes.ul}>
+            {items.map((x) => (
+              <li className={classes.li}>
+                <p>
+                  <span>{x.itemName}</span> <span>{x.quantity}</span> x{" "}
+                  <span>{x.price}$</span>
+                </p>
+              </li>
+            ))}
+          </ul>
+          <div className={classes.total}>
+            <p>Total Amount: {props.total}$</p>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <button className={classes.btn_cancel} onClick={props.closeModal}>
+            Cancel
+          </button>
+          <button className={classes.btn_order}>Order</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CartModal;
