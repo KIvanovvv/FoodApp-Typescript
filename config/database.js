ed4399e6bbb56b7057ba1a2c77e36af2c65a6e5c
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-04-05 15:22:01 +0300

    Test

--- config/database.js
+++ config/database.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
+const MONGOURI = `mongodb+srv://kivanov:GBlcgm8P5UmOVhRU@cluster0.gkosfhk.mongodb.net/test`;
 
 async function runDB() {
   mongoose.set("strictQuery", true);
   try {
-    await mongoose.connect(
-      `mongodb+srv://kivanov:GBlcgm8P5UmOVhRU@cluster0.gkosfhk.mongodb.net/test`
-    );
+    await mongoose.connect(MONGOURI);
     console.log(`Database is connected`);
   } catch (err) {
     console.log(err);
     process.exit(1);
   }
 }
-
 module.exports = runDB;
