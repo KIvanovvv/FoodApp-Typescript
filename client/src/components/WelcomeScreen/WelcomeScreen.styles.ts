d07dc38fba42138c30dac7dfab5df2c8a621088a
Shtono <shtonojs@gmail.com>
2023-04-15 02:18:45 +0300

    initial refactoring

---
+++ client/src/components/WelcomeScreen/WelcomeScreen.styles.ts
@@ -0,0 +1,19 @@
+// .wrapper {
+//   background-color: #d62828;
+//   height: 919px;
+//   padding-bottom: 50px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   gap: 60px;
+// }
+
+const wrapper = {
+  backgroundColor: '#d62828',
+  height: '919px',
+  paddingBottom: '50px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '60px',
+}
